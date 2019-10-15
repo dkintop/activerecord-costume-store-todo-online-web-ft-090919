df4f741c3a09f727db6b60fad66d5483875a02be
Doug Kintop <dkintop@hotmail.com>
2019-10-15 13:15:23 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,5 +7,6 @@ class CreateCostumeStores < ActiveRecord::Migration
       t.string :location
       t.integer :costume_inventory
       t.integer :num_of_employees
+      t.boolean :still_in_business
       
 end 
